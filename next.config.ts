5c0b7a035146e484a586999fa4ff5e186d56a6de
sabbir073 <md.sabbir073@gmail.com>
2025-08-18 18:36:45 +0600

    cconfig changed

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dahf45b8zc9m5.cloudfront.net',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
+  },
   async headers() {
     return [
       {
